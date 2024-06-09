563fdd395d1f27e29e310dfecef110369287cd38
giovanavbs <giovana.santos261@etec.sp.gov.br>
2024-06-09 03:25:09 -0300

    login/cadastro bonitinho

--- lib/login.dart
+++ lib/login.dart
@@ -30,7 +30,7 @@ class Login extends StatelessWidget {
             Positioned.fill( 
               top: 80, // o form de login nao fica por baixo do menuSuperior
               child: Center(
-                child: loginForm(),
+                child: LoginForm(),
               ),
             ),
             Positioned(
@@ -48,10 +48,232 @@ class Login extends StatelessWidget {
 }
 
 
-// classe com o layout de login
+// classe com o layout de login, efeito de trocar de aba pra login/cadastro só por estetica e teste de efeitos no flutter
 
-class loginForm extends StatelessWidget {
-  const loginForm ({super.key});
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> with SingleTickerProviderStateMixin {
+  late TabController abas; // a aba de login/cadastro
+  late PageController pag; // a pagina que é exibida na troca de aba de login para cadastro viceversa
+
+  @override
+  void initState() {
+    super.initState();
+    abas = TabController(length: 2, vsync: this); // vsync sincroniza verticalmente a animação com a taxa de atualização do dispositivo, nesse caso o this determina que esse elemento sera usado como parametro de atualização
+    pag = PageController();
+
+    abas.addListener(() {
+      pag.animateToPage(
+        abas.index,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.ease,
+      ); // efeito da animação da troca de aba
+    });
+  }
+
+  @override
+  void dispose() {
+    abas.dispose();
+    pag.dispose();
+    super.dispose();
+  } 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+              child: TabBar( 
+                controller: abas,
+                indicator: BoxDecoration(
+                  borderRadius: BorderRadius.circular(25),
+                  color: Colors.white,
+                ),
+                labelColor: Colors.black,
+                unselectedLabelColor: Colors.white,
+                tabs: const [
+                  Tab(text: 'LOGIN'),
+                  Tab(text: 'CADASTRO'),
+                ],
+              ),
+            ),
+            Expanded(
+              child: PageView(
+                controller: pag,
+                onPageChanged: (index) {
+                  abas.animateTo(index);
+                },
+                children: [
+                  FormLogin(),
+                  FormCadastro(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget FormLogin() {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Text(
+            'Usuário',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 10),
+          TextFormField(
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.person),
+              hintText: 'Digite o usuário',
+              filled: true,
+              fillColor: Colors.grey[200],
+              border: const OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                borderSide: BorderSide.none,
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          const Text(
+            'Senha',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 10),
+          TextFormField(
+            obscureText: true,
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.lock),
+              hintText: 'Digite a senha',
+              filled: true,
+              fillColor: Colors.grey[200],
+              border: const OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                borderSide: BorderSide.none,
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              backgroundColor: Colors.blue,
+            ),
+            onPressed: () {
+              // ação de login 
+            },
+            child: const Text(
+              'Login',
+              style: TextStyle(fontSize: 16, color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget FormCadastro() {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Text(
+            'Nome',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 10),
+          TextFormField(
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.person),
+              hintText: 'Digite seu nome',
+              filled: true,
+              fillColor: Colors.grey[200],
+              border: const OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                borderSide: BorderSide.none,
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          const Text(
+            'Usuário',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 10),
+          TextFormField(
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.person),
+              hintText: 'Digite o usuário',
+              filled: true,
+              fillColor: Colors.grey[200],
+              border: const OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                borderSide: BorderSide.none,
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          const Text(
+            'Senha',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 10),
+          TextFormField(
+            obscureText: true,
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.lock),
+              hintText: 'Digite a senha',
+              filled: true,
+              fillColor: Colors.grey[200],
+              border: const OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                borderSide: BorderSide.none,
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              backgroundColor: Colors.blue,
+            ),
+            onPressed: () {
+              // ação de cadastro
+            },
+            child: const Text(
+              'Cadastrar',
+              style: TextStyle(fontSize: 16, color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+/* antigo login sem a animação
+class LoginForm extends StatelessWidget {
+  const LoginForm({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -61,36 +283,61 @@ class loginForm extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
-            margin: const EdgeInsets.only(bottom: 10), // espaço entre o texto usuario e o text form field pra n ficar colado
+            margin: const EdgeInsets.only(bottom: 10),
             child: const Text(
-              'usuario',
+              'Usuário',
               style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
             ),
           ),
           TextFormField(
-            decoration: const InputDecoration( // sugere usar o inputdecoration pra colocar o hint
-              hintText: 'digite o usuario',
+            decoration: InputDecoration(
+              prefixIcon: const Icon(Icons.person),
+              hintText: 'Digite o usuário',
+              filled: true,
+              fillColor: Colors.grey[200],
+              border: const OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                borderSide: BorderSide.none,
+              ),
             ),
           ),
           Container(
-            margin: EdgeInsets.symmetric(vertical: 20), // margin entre o dois text form field pra n ficar colado
+            margin: const EdgeInsets.symmetric(vertical: 20),
             child: const Text(
-              'senha',
+              'Senha',
               style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
             ),
           ),
           TextFormField(
-            decoration: const InputDecoration(
-              hintText: 'digite a senha',
+            obscureText: true,
+            decoration: InputDecoration(
+              prefixIcon: const Icon(Icons.lock),
+              hintText: 'Digite a senha',
+              filled: true,
+              fillColor: Colors.grey[200],
+              border: const OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                borderSide: BorderSide.none,
+              ),
             ),
           ),
           Container(
-            margin: EdgeInsets.symmetric(vertical: 20), // espaço pro botão login n colar 
+            margin: const EdgeInsets.symmetric(vertical: 20),
             child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                backgroundColor: const Color(0xFF0072ce),
+              ),
               onPressed: () {
-                // sem função de login por enquanto
+                // ação de login
               },
-              child: Text('login'),
+              child: const Text(
+                'Login',
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
             ),
           ),
         ],
@@ -98,3 +345,4 @@ class loginForm extends StatelessWidget {
     );
   }
 }
+*/
